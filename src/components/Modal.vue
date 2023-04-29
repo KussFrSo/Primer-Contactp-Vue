66ed317562d9a14690188e029f8dfc81ed67caa9
KussFrSo <marcfranch.fh@gmail.com>
2023-04-29 22:10:38 +0200

    Teleports

---
+++ src/components/Modal.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <button @click="toggle">Modal</button>
+    <teleport to="body">
+      <div v-show="show" class="modal">
+        <h1>Titulo</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. A cumque
+          magni obcaecati pariatur corporis ab vitae maiores ratione porro nemo
+          est officia nobis praesentium quis voluptatibus, voluptates hic
+          aperiam. Officiis?
+        </p>
+        <button @click="toggle">Cerrar</button>
+      </div>
+    </teleport>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      show: false,
+    };
+  },
+  methods: {
+    toggle() {
+      this.show = !this.show;
+    },
+  },
+};
+</script>
